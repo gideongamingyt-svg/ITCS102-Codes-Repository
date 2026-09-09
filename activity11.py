6a82b3e8ae0ebf8e402407c12b61fb2facc86e7b
Gideon Palciso <gideongamingyt@gmail.com>
2026-09-09 18:19:03 +0800

    Add files via upload

---
+++ activity11.py
@@ -0,0 +1,12 @@
+import getpass 
+
+username = 'palcisomewing'
+password = 'handsomemewing'
+
+u = input('Input Username ---> ')
+p = input('Input Password ---> ')
+
+if username == u and p == password : 			
+	print("ACCESS GRANTED")
+else: 
+	print("ACCESS DENIED")
